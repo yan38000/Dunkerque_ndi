8f12623bb54cfbfc8ed13ff1d2aa5ae8c03276bc
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-12-02 20:26:17 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,17 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : process.env.LOCALHOST,
+    user     : process.env.USER,
+    password : process.env.PASSWORD
+    
+})
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting');
+      return;
+    }
+   
+    console.log('connected');
+})
