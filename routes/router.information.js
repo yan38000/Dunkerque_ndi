1a6a54942484bd93326507ff1cca7c231845ca08
yan38000 <61967417+yan38000@users.noreply.github.com>
2021-12-02 21:26:39 +0100

    yan38000
    router

--- routes/router.information.js
+++ routes/router.information.js
@@ -3,5 +3,7 @@ const controllerAllInformation = require('../controllers/controllers.information
 
 
 router.get('/allInfo' , controllerAllInformation.AllInformation)
-
+router.get('/:id' , controllerAllInformation.ShearchInformation)
+router.post('/addInfo' , controllerAllInformation.AddInformation)
+router.put('/updateInfo', controllerAllInformation.UpdateInformation)
 module.exports = router;
