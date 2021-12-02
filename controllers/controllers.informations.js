6724a3464f66b8181308c73d82268a090f89e3e9
VicMls38 <72078263+VicMls38@users.noreply.github.com>
2021-12-02 21:34:59 +0100

    module exports

--- controllers/controllers.informations.js
+++ controllers/controllers.informations.js
@@ -1,12 +1,18 @@
-module.exports.AllInformation = async (req, res)=>{
+module.exports = {    
+
+    AllInformation = async (req, res)=>{
     res.send('helle')
-}
-module.exports.ShearchInformation = async (req, res)=>{
-    res.send('helle')
-}
-module.exports.AddInformation = async (req, res)=>{
-    res.send('helle')
-}
-module.exports.UpdateInformation = async (req, res)=>{
-    res.send('helle')
+    },
+
+    ShearchInformation : async (req, res)=>{
+        res.send('helle')
+    },
+
+    AddInformation : async (req, res)=>{
+        res.send('helle')
+    },
+
+    UpdateInformation :  async (req, res)=>{
+        res.send('helle')
+    },
 }
