f00d7b2e5007c3f372851089e50d11f038fff465
VicMls38 <72078263+VicMls38@users.noreply.github.com>
2021-12-03 05:22:03 +0100

    Delete routes directory

--- routes/router.information.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controllerAllInformation = require('../controllers/controllers.informations')
-
-
-router.get('/allInfo' , controllerAllInformation.All_Info)
-router.get('/:id' , controllerAllInformation.ShearchInformation)
-router.post('/addInfo' , controllerAllInformation.AddInformation)
-router.put('/updateInfo', controllerAllInformation.UpdateInformation)
-module.exports = router;
